8bdfe706abf92a4eb1670f6139269676644a47ea
snow <snow@sidecar.co.jp>
2021-10-16 17:14:02 +0900

    nabeliwo

---
+++ react-todo/src/components/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Form from "./Form";
+import Todo from "./Todo";
+
+let currentId = 0;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Form onSubmit0={this.handleSubmit} />
+
+        <label>
+          <input type="checkbox" />
+          全て完了にする
+        </label>
+
+        <select>
+          <option>全て</option>
+          <option>未完了</option>
+          <option>完了済</option>
+        </select>
+
+        <ul>
+          {this.state.todos.map(({ id, text, completed }) => (
+            <li key={id}>
+              <Todo
+                id={id}
+                text={text}
+                completed={completed}
+                onChange={this.handleChangeCompleted}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <button>完了済を全て削除</button>
+      </div>
+    );
+  }
+
+  handleSubmit = (text) => {
+    const newTodo = {
+      id: currentId,
+      text,
+      completed: false,
+    };
+    const newTodos = [...this.state.todos, newTodo];
+    this.setState({ todos: newTodos });
+    currentId++;
+  };
+
+  handleChangeCompleted = (id, completed) => {
+    const newTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed,
+        };
+      }
+
+      return todo;
+    });
+    this.setState({ todos: newTodos });
+  };
+}
+
+export default App;
---
+++ react-todo/src/components/Todo.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class Todo extends React.Component {
+  render() {
+    const { text, completed } = this.props;
+
+    return (
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={completed}
+            onChange={this.handleChangeCompleted}
+          />
+          {text}
+        </label>
+        <button>編集</button>
+        <button>削除</button>
+      </div>
+    );
+  }
+
+  handleChangeCompleted = () => {
+    const { onChange, id, completed } = this.props;
+    onChange(id, !completed);
+  };
+}
+
+export default Todo;
